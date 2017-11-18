9b0f6fc79833ee43005da2269653b8463ab2fd80
Xinyu Xieyang <yx4247@utexas.edu>
2017-11-17 22:03:47 -0600

    Ver17Nov2017

--- Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
+++ Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
@@ -8,98 +8,105 @@ using Random = UnityEngine.Random;
 
 namespace UnityStandardAssets.Characters.FirstPerson
 {
-    [RequireComponent(typeof (CharacterController))]
-    public class FirstPersonController : MonoBehaviour
-    {
-        [SerializeField] private MouseLook m_MouseLook;
+	[RequireComponent(typeof (CharacterController))]
+	public class FirstPersonController : MonoBehaviour
+	{
+		[SerializeField] private MouseLook m_MouseLook;
 
-        private Camera m_Camera;
-        private Vector3 m_MoveDir = Vector3.zero;
-        private CharacterController m_CharacterController;
-        private float m_StepCycle;
+		private Camera m_Camera;
+		private Vector3 m_MoveDir = Vector3.zero;
+		private CharacterController m_CharacterController;
+		private float m_StepCycle;
+		public float Bx;
+		public float By;
+		public float Bz;
+		public float speed;
 
 		public Text countText;
 		private int count;
 
-        // Use this for initialization
-        private void Start()
-        {
-            m_CharacterController = GetComponent<CharacterController>();
-            m_Camera = Camera.main;
-            m_MouseLook.Init(transform , m_Camera.transform);
+		// Use this for initialization
+		private void Start()
+		{
+			m_CharacterController = GetComponent<CharacterController>();
+			m_Camera = Camera.main;
+			m_MouseLook.Init(transform , m_Camera.transform);
 			count = 0;
 			SetCountText ();
-        }
+		}
 
 
-        // Update is called once per frame
-        private void Update()
-        {
+		// Update is called once per frame
+		private void Update()
+		{
 			Vector3 moveDirection = m_Camera.transform.forward;
 			RotateView();
-        }
+		}
 
-        private void FixedUpdate()
-        {
-			//swimming speed
-			float speed = 0.5f;
+		private void FixedUpdate()
+		{
 			Vector3 moveDirection = m_Camera.transform.forward;
 			Vector3 presentPosition = m_Camera.transform.position;
 
 			//Boundaries of limited swimming spaces
-			if (presentPosition.y > 42)
-				moveDirection.y = 0f;
-			if (presentPosition.y < 1)
-				moveDirection.y = 0f;
-			if (presentPosition.x > 50)
-				moveDirection.x = 0f;
-			if (presentPosition.x < -50)
-				moveDirection.x = 0f;
-			if (presentPosition.z > 50)
-				moveDirection.z = 0f;
-			if (presentPosition.z < -50)
-				moveDirection.z = 0f;
+			if (presentPosition.y > By || presentPosition.y < 1 || presentPosition.x > Bx || presentPosition.x < 0 || presentPosition.z > Bz || presentPosition.z < 0) {
+				countText.text = "Boundary Warning";
+				if (presentPosition.y > By && moveDirection.y > 0)
+					moveDirection.y = 0f; 
+				if (presentPosition.y < 1 && moveDirection.y < 0)
+					moveDirection.y = 0f;
+				if (presentPosition.x > Bx && moveDirection.x > 0) 
+					moveDirection.x = 0f;
+				if (presentPosition.x < 0 && moveDirection.x < 0)
+					moveDirection.x = 0f;
+				if (presentPosition.z > Bz && moveDirection.z > 0)
+					moveDirection.z = 0f;
+				if (presentPosition.z < 0 && moveDirection.z < 0)
+					moveDirection.z = 0f;
+			}else {
+				SetCountText ();
+			}
 
 			//Derection setup
 			m_MoveDir.x = moveDirection.x*speed;
 			m_MoveDir.z = moveDirection.z*speed;
 			m_MoveDir.y = moveDirection.y*speed;
 
-            m_CharacterController.Move(m_MoveDir*Time.fixedDeltaTime);
+			m_CharacterController.Move(m_MoveDir * Time.fixedDeltaTime);
 
-            UpdateCameraPosition(speed);
+			UpdateCameraPosition(speed);
 
-            m_MouseLook.UpdateCursorLock();
-        }
-			
+			m_MouseLook.UpdateCursorLock();
+		}
 
-        private void UpdateCameraPosition(float speed)
-        {
-            Vector3 newCameraPosition;
-            newCameraPosition = m_Camera.transform.localPosition;
-            m_Camera.transform.localPosition = newCameraPosition;
-        }
+
+		private void UpdateCameraPosition(float speed)
+		{
+			Vector3 newCameraPosition;
+			newCameraPosition = m_Camera.transform.localPosition;
+			m_Camera.transform.localPosition = newCameraPosition;
+		}
 
 		private void RotateView()
-        {
-            m_MouseLook.LookRotation (transform, m_Camera.transform);
-        }
+		{
+			m_MouseLook.LookRotation (transform, m_Camera.transform);
+		}
 
 
-        private void OnControllerColliderHit(ControllerColliderHit hit)
-        {
+		private void OnControllerColliderHit(ControllerColliderHit hit)
+		{
 			//Getting a bubble
 			if (hit.collider.tag == "Pick up")
-            {
+			{
 				count++;
 				SetCountText ();
 				hit.gameObject.SetActive(false);
 			}
-        }
+		}
 
 		private void SetCountText ()
 		{
 			countText.text = "Count: " + count.ToString ();
 		}
-    }
+	}
 }
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # SwimmingPool
+
+Version 17 Nov 2017
+Yangxinyu Xie
+
+Smoother Swimming System
+Better Scripts
