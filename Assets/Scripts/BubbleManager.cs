6488b37f4deae4e9c4c1d8ec4efbd6a19f6bc0e2
Ricardo <rivezu97@gmail.com>
2017-11-26 17:39:08 -0600

    Randomization Improvements
    New Branch: Randomization
    
    Updates:
    1. Improved the randomization algorithm
    2. Added the option to modify the initial amount of bubbles
    3. Added some lines to spawn a bubble when you hit an old bubble

--- Assets/Scripts/BubbleManager.cs
+++ Assets/Scripts/BubbleManager.cs
@@ -5,18 +5,32 @@ public class BubbleManager : MonoBehaviour
 	public GameObject bubble;                // The enemy prefab to be spawned.
 	public float spawnTime = 3f;            // How long between each spawn.
 	public Transform spawnPoints;         // An array of the spawn points this enemy can spawn from.
+    public float initBubbles;           //Initial amount of bubbbles
+    public float Bx;
+    public float By;
+    public float Bz;
 
-
-	void Start ()
+    void Start ()
 	{
-		// Call the Spawn function after a delay of the spawnTime and then continue to call after the same amount of time.
-		InvokeRepeating ("Spawn", spawnTime, spawnTime);
-	}
+        // Call the Spawn function after a delay of the spawnTime and then continue to call after the same amount of time.
+        for (int i = 0; i < initBubbles; i++)
+        {
+            Invoke("Spawn", spawnTime);
+        }
+    }
 
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        Spawn();
+    }
 
-	void Spawn ()
+    void Spawn ()
 	{
-		// Create an instance of the bubble prefab at the randomly selected spawn point's position and rotation.
-		Instantiate (bubble, spawnPoints.position, spawnPoints.rotation);
+        Vector3 pos = new Vector3();
+        pos.x = Random.Range(1f, Bx - 1);
+        pos.y = Random.Range(1f, By - 1);
+        pos.z = Random.Range(1f, Bz - 1);
+        // Create an instance of the bubble prefab at the randomly selected spawn point's position and rotation.
+        Instantiate (bubble, pos, Quaternion.identity);
 	}
 }
