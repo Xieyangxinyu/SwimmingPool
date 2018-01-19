9f4f0f574e7e07631d6682eaaed912c0e0322e59
Xinyu Xieyang <yx4247@utexas.edu>
2018-01-19 14:51:53 -0600

    Ver10Jan2018

--- Assets/Scripts/BubbleManager.cs
+++ Assets/Scripts/BubbleManager.cs
@@ -3,12 +3,11 @@
 public class BubbleManager : MonoBehaviour
 {
 	public GameObject bubble;                // The bubble prefab to be spawned.
-	public float spawnTime = 3f;            // How long between each spawn.
-	public Transform spawnPoints;         // An array of the spawn points this bubble can spawn from.
-    public float initBubbles;           //Initial amount of bubbles
+    public int initBubbles;           //Initial amount of bubbles
 	public float Bx;                   //Bx,By and Bz are boundary-control-variables
     public float By;
     public float Bz;
+	public GameObject[] Fishes;
 
 	// You may edit this entire thing to make it a read-file function to import maps
 	// More information about spawn can be found on Unity Tutorial Series
@@ -17,22 +16,18 @@ public class BubbleManager : MonoBehaviour
         // Call the Spawn function after a delay of the spawnTime and then continue to call after the same amount of time.
         for (int i = 0; i < initBubbles; i++)
         {
-            Invoke("Spawn", spawnTime);
+			Spawn (i);
         }
     }
 
-    private void OnControllerColliderHit(ControllerColliderHit hit)
-    {
-        Spawn();
-    }
-
-    void Spawn ()
+	void Spawn (int fishNumber)
 	{
         Vector3 pos = new Vector3();
         pos.x = Random.Range(1f, Bx - 1);
         pos.y = Random.Range(1f, By - 1);
         pos.z = Random.Range(1f, Bz - 1);
         // Create an instance of the bubble prefab at the randomly selected spawn point's position and rotation.
-        Instantiate (bubble, pos, Quaternion.identity);
+        GameObject thisbubble = Instantiate (bubble, pos, Quaternion.identity);
+		Instantiate (Fishes [fishNumber], pos, Quaternion.identity).transform.SetParent (thisbubble.transform);
 	}
 }
---
+++ Assets/Scripts/nav.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class nav : MonoBehaviour {
+
+	public GameObject target;
+	NavMeshAgent navi;
+	// Use this for initialization
+	void Start () {
+		navi = GetComponent<NavMeshAgent> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		navi.SetDestination (target.transform.position);
+	}
+}
