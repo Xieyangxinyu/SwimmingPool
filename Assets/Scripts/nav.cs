71133708c0c40325848074bebe4e87f03cd832ca
Xinyu Xieyang <yx4247@utexas.edu>
2018-02-02 17:14:01 -0600

    Ver2Feb2018

--- Assets/Scripts/nav.cs
+++ Assets/Scripts/nav.cs
@@ -6,16 +6,16 @@ using UnityEngine.AI;
 public class nav : MonoBehaviour {
 
 	public GameObject target;
-	NavMeshAgent navi;
+	public float rotationSpeed;
+	public float moveSpeed;
 	// Use this for initialization
 	void Start () {
-		navi = GetComponent <NavMeshAgent> ();
-		navi.SetDestination (new Vector3 (0, 0, 0));
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		navi.SetDestination (target.transform.position);
-		this.transform.rotation = target.transform.rotation;
+		transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(target.transform.position - transform.position), rotationSpeed * Time.deltaTime);
+		transform.position += transform.forward * Time.deltaTime * moveSpeed;
 	}
 }
